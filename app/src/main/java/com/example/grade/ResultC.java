3e6f3921a60fa062454d4ec42203f0c5ffe4f4ab
你的名字 <email>
2019-05-31 00:42:15 +0800

    HW finish

--- app/src/main/java/com/example/grade/ResultC.java
+++ app/src/main/java/com/example/grade/ResultC.java
@@ -1,6 +1,7 @@
 package com.example.grade;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -31,9 +32,36 @@ public class ResultC extends AppCompatActivity {
         String text="programming= "+prog+"\ndata structure= "+struct+"\nAlgorithm= "+algo+"\nsum= "+sum+"\nAverage= "+nf.format(avg);
         ans.setText(text);
         ans.setTextSize(25);
+        alert(avg);
     }
 
     public void close(View v){
         finish();
+    }
+
+    private void alert(double avg){
+        String text=new String();
+        String title=new String();
+        int pic;
+
+        if(avg>=60){
+            text="恭喜您及格了!!!";
+            title="Pass!";
+            pic=R.drawable.pass;
+        }
+        else{
+            text="天啊你不及格....GGGGGG...";
+            title="It's very sad....QQ";
+            pic=R.drawable.fail;
+        }
+
+        AlertDialog.Builder build=new AlertDialog.Builder(this);
+        build.setMessage(text);
+        build.setTitle(title);
+        build.setIcon(pic);
+        build.setPositiveButton("OK",null);
+        build.setNegativeButton("Cancel",null);
+        build.setNeutralButton("Nothing",null);
+        build.show();
     }
 }
